91e276315c00c2753d2d0cd9d7940d2947b24040
Arnoldo <arnoldo.cortez@theksquaregroup.com>
2021-09-30 12:27:32 -0500

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JfRt6JRE4qQc6WNRWgQC8pkYdLsH2oLhlSpJ7jzlG0sx6hKlJyk1stCaJs2jGksgfHeJ8mTOQzXARQnXM2lX4XH00h79EnMKW";
+
+  const onToken = (token) => {
+    alert("Payment Succesful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https:\\svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
